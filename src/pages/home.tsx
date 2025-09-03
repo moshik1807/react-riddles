13f74d9a159abb4a3574ff0be987dfd3a9a0106f
moshik <moshik0527669169@gmail.com>
2025-09-03 09:14:51 +0300

    x

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Link, useNavigate } from "react-router";
 import Login from "./login";
+import { useState } from "react";
 
 export default function Home(){
     const navigate=useNavigate()
+    const[token,setToken]=useState()
     return(
         <>
         <h1>home</h1>
