d28cd612b9eadadbcbb1447127c0a643a31b1f1e
moshik <moshik0527669169@gmail.com>
2025-09-03 17:07:31 +0300

    add updeate riddle

--- src/pages/updeateRiddle.tsx
+++ src/pages/updeateRiddle.tsx
@@ -1,13 +1,40 @@
+import { useRef } from "react"
+import { loadToken } from "../util/localstorege"
 export default function updeate(){
+    const form = useRef<HTMLFormElement>(null)
+    const id=useRef<HTMLInputElement>(null)
+    const name=useRef<HTMLInputElement>(null)
+    const taskDescription=useRef<HTMLInputElement>(null)
+    const correctAnswer=useRef<HTMLInputElement>(null)
+    const hint=useRef<HTMLInputElement>(null)
+    
+    const stored = loadToken("authToken"); 
+
     return(
         <>
         <h1>updeate</h1>
-        <form action="">
-            <input type="text" name="id" placeholder="enter id riddle you whant to change"/>
-            <input type="text" name="taskDescription" placeholder="enter taskDescription updeat"/>
-            <input type="text" name="correctAnswer" placeholder="enter correctAnswer updeat"/>
-            <input type="text" name="hint" placeholder="enter hint updeat" />
-            <button type="submit">submit</button>
+        <form ref={form} action="">
+            <input type="text" name="id" placeholder="enter id" ref={id}/>
+            <input type="text" name="name" placeholder="enter name" ref={name}/>
+            <input type="text" name="taskDescription" placeholder="enter taskDescription" ref={taskDescription}/>
+            <input type="text" name="correctAnswer" placeholder="enter correctAnswer" ref={correctAnswer}/>
+            <input type="text" name="hint" placeholder="enter hint" ref={hint}/>
+            <button type="submit" onClick={(e)=>{
+                e.preventDefault();
+                const formData = new FormData(form.current as HTMLFormElement);
+                const riddle: Record<string, any> = {};
+
+                formData.forEach((value, key) => {
+                    riddle[key] = value;  
+                });
+                fetch('http://localhost:2123/riddle/updeate', {
+                    method: 'PUT',
+                    headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization':'Bearer ' + stored},
+                    body: JSON.stringify(riddle)
+                })
+                }}>submit</button>
         </form>
         </>
     )
